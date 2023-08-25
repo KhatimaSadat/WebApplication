0cb2da4799ae96c4adb2a53b80093169acdba5fc
Khatima Sadat <khatimasadat70@gmail.com>
2023-08-26 01:03:31 +0430

    Updated with Larkhod

---
+++ WebSite/src/App/Book.js
@@ -0,0 +1,232 @@
+import { AuthContext } from "./context/AuthContext";
+import { useContext } from 'react';
+import { useNavigate } from "react-router-dom";
+import i18n from '../i18n';
+import { withNamespaces } from 'react-i18next';
+import * as React from "react"
+import { useRef } from 'react';
+import JSONFiles from './Json Files/SchoolSubjects.json';
+import { Link, useLocation } from "react-router-dom"
+import image5 from "./assets/Group_158_a@2x.png";
+import sanaf from "./assets/sanaf.png";
+import bookH from "./assets/Group 463.png";
+import pathimg from "./assets/Path 968.png";
+import line from "./assets/Line 117.png";
+import JsonFiles from "./Json Files/SchoolSubjects.json";
+
+import './books.css';
+const base_path = "https://larkhoad.s3.ap-south-1.amazonaws.com";
+const Book = ({ t }) => {
+  const { userInfo, isLoading } = useContext(AuthContext);
+  var data;
+  if (t("sectionlang") == "dari") {
+    data = JsonFiles.dari_curriculam;
+  } else {
+    data = JsonFiles.pashto_curriculam;
+  }
+
+  const changeLanguage = (lng) => {
+    i18n.changeLanguage(lng);
+  }
+
+  var primary = data.primary.grades;
+  var primary_path = base_path + data.primary.base_path;
+  var secondary = data.secondary.grades;
+  var secondary_path = base_path + data.secondary.base_path;
+  var high_school = data.high_school.grades;
+  var high_school_path = base_path + data.high_school.base_path;
+
+  const [srollState, setScrollState] = React.useState();
+  const bottom = useRef(null);
+  const top = useRef(null)
+  const scrollBottom = () => {
+    bottom.current.scrollIntoView({ behavier: "smooth" });
+    setScrollState("bottom")
+
+  }
+  const scrollTop = () => {
+    top.current.scrollIntoView({ behavier: "smooth" });
+    setScrollState("top")
+  }
+
+  // const navigation = useNavigate();
+  //   if (userInfo === null) {
+  //     return navigation("/");
+  //   } else if (isLoading) {
+  //     return <h1>Loading.....</h1>;
+  //   } else if(userInfo !== null){
+  return (
+    <div className='Download-all-b'>
+      <div className="Bookheader">
+        <div id="flesh-image-grade-B" > <Link to="/Header"><img className="flesh-image-grade-B" src={image5} /></Link>
+        </div>
+        <div className="second-section">
+          <div className="icon-book">
+            <img src={bookH} className="bookH" />
+          </div>
+          <div className='buttom-books'>
+            <div className='p-container-books'>
+
+
+              {/* <p className='p-books'> کتاب های مورد نظر صنف خود را</p>
+         <p id='p-books'>از اینجا انتخاب  کنید</p> */}
+              {/* <p className='p-books'>{t("book1")}</p> */}
+
+              <p className='p-books'>{t("book1")}</p>
+              <p id='p-books'> {t("book4")}</p>
+            </div>
+            <div id='h-books'>
+              <div ref={top} >
+                {primary.map((item) => {
+
+                  return (
+                    <Link
+                      className="link-grade-nb"
+                      to="/downloading"
+                      state={{ subjects: item.subjects, grade_path: primary_path + item.grade_path, grade_name: item.label }}
+                    >
+                      <div className="sanaf-grade-nb"> <img src={sanaf} /></div>
+                      <div className="li-grade-bb" src={sanaf} >{t("book2")} {item.label} {t("book3")}</div>
+                    </Link>
+                  );
+                })}
+              </div>
+              <div ref={bottom}>
+                {secondary.map((item) => {
+                  return (
+                    <Link
+                      className="link-grade-nb"
+                      to="/downloading"
+                      state={{ subjects: item.subjects, grade_path: secondary_path + item.grade_path, grade_name: item.label }}
+                    >
+                      <div className="sanaf-grade-nb"> <img src={sanaf} /></div>
+
+                      {/* // <div className="li-grade-bb" src={sanaf}>{t("book2")} {item.label}</div> */}
+
+                      <div className="li-grade-bb" src={sanaf}>{t("book2")} {item.label} {t("book3")}</div>
+
+                    </Link>
+                  );
+                })}
+              </div>
+              <div >
+                {high_school.map((item) => {
+                  return (
+                    <Link
+                      className="link-grade-nb"
+                      to="/downloading"
+                      state={{ subjects: item.subjects, grade_path: high_school_path + item.grade_path, grade_name: item.label }}
+                    >
+                      <div className="sanaf-grade-nb"> <img src={sanaf} /></div>
+                      {/*  <div className="li-grade-bb" src={sanaf}> {t("book2")}  {item.label}</div> */}
+
+                      <div className="li-grade-bb" src={sanaf}> {t("book2")}  {item.label} {t("book3")}</div>
+
+                    </Link>
+                  );
+                })}
+              </div>
+            </div>
+
+            <div id="line">
+              <img src={line} className="line" />
+            </div>
+            <div id="line">
+              <img src={pathimg} className="pathimg-b" onClick={() => {
+                if (srollState == "bottom") {
+                  scrollTop()
+                } else {
+                  scrollBottom()
+                }
+              }} />
+            </div>
+          </div>
+        </div>
+
+      </div>
+      <div className="BookheaderM">
+        <div id="flesh-image-grade-BM" > <Link to="/Header"><img className="flesh-image-grade-B" src={image5} /></Link> </div>
+        <div className="second-section-m">
+          <div className='p-container-booksM'>
+
+            <p className='p-books'>{t("book1")}</p>
+            <p id='p-books'> {t("book4")}</p>
+          </div>
+          <div className='buttom-booksM'>
+            <div id='h-booksM'>
+              <div ref={top} >
+                {primary.map((item) => {
+
+                  return (
+                    <Link
+                      className="link-grade-nm"
+                      to="/downloading"
+                      state={{ subjects: item.subjects, grade_path: primary_path + item.grade_path, grade_name: item.label }}
+                    >
+                      <div className="sanaf-grade-nm"> <img src={sanaf} /></div>
+                      <div className="li-grade-b" src={sanaf} >{t("book2")} {item.label} {t("book3")}</div>
+                    </Link>
+                  );
+                })}
+              </div>
+              <div ref={bottom}>
+                {secondary.map((item) => {
+                  return (
+                    <Link
+                      className="link-grade-nm"
+                      to="/downloading"
+                      state={{ subjects: item.subjects, grade_path: secondary_path + item.grade_path, grade_name: item.label }}
+                    >
+                      <div className="sanaf-grade-nm"> <img src={sanaf} /></div>
+                      {/* // <div className="li-grade-b" src={sanaf}>{t("book2")} {item.label}</div> */}
+
+                      <div className="li-grade-b" src={sanaf}>{t("book2")} {item.label} {t("book3")}</div>
+
+                    </Link>
+                  );
+                })}
+              </div>
+              <div >
+                {high_school.map((item) => {
+                  return (
+                    <Link
+                      className="link-grade-nm"
+                      to="/downloading"
+                      state={{ subjects: item.subjects, grade_path: high_school_path + item.grade_path, grade_name: item.label }}
+                    >
+                      <div className="sanaf-grade-nm"> <img src={sanaf} /></div>
+                      {/*  <div className="li-grade-b" src={sanaf}> {t("book2")}  {item.label}</div> */}
+
+                      <div className="li-grade-b" src={sanaf}> {t("book2")}  {item.label} {t("book3")}</div>
+
+                    </Link>
+                  );
+                })}
+              </div>
+            </div>
+
+            <div id="line">
+              <img src={line} className="line" />
+            </div>
+
+            <div id="line">
+              <img src={pathimg} className="pathimg-bm" onClick={() => {
+                if (srollState == "bottom") {
+                  scrollTop()
+                } else {
+                  scrollBottom()
+                }
+              }} />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="icon-book-m">
+        <img src={bookH} />
+      </div>
+
+    </div>
+  )
+  // }
+}
+export default withNamespaces()(Book);
---
+++ WebSite/src/App/screens/Login/LogIn.js
@@ -0,0 +1,179 @@
+import { AuthContext } from "../../context/AuthContext";
+import { useContext } from "react";
+import { Link, useNavigate, useNavigation } from "react-router-dom";
+import { withNamespaces } from "react-i18next";
+import { useGoogleLogin } from "@react-oauth/google";
+import axios from "axios";
+import "./LogIn.css";
+import { FaGoogle } from "react-icons/fa";
+import { useState, useEffect } from "react";
+import img1 from "../../assets/mg.png";
+import React from "react";
+import img3 from "../../assets/logo_3.png";
+<link rel="stylesheet" href="LogIn.css" />;
+
+const LogIn = ({ t }, props) => {
+  const loggin = useGoogleLogin({
+    onSuccess: async (respose) => {
+      try {
+        const res = await axios.get(
+          "https://www.googleapis.com/oauth2/v3/userinfo",
+          {
+            headers: {
+              Authorization: `Bearer ${respose.access_token}`,
+            },
+          }
+        );
+        console.log(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  });
+  const { isLoading, login, userInfo, backendError } = useContext(AuthContext);
+  const initialValues = {
+    email: "",
+    password: "",
+  };
+  const [formValues, setFormValues] = useState(initialValues);
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormErrors(validate(formValues));
+    console.log(formValues.email, formValues.password);
+    login(formValues.email, formValues.password);
+    setIsSubmit(true);
+    if (Object.keys(formErrors).length === 0 && isSubmit) {
+      navigate("/header");
+    }
+    // {
+    //   Object.keys(formErrors).length === 0 && isSubmit ? (
+    //     <div className="ui message success">{navigate("/header")}</div>
+    //   ) : (
+    //     <pre></pre>
+    //   );
+    // }
+  };
+  useEffect(() => {
+    console.log(formErrors);
+    if (Object.keys(formErrors).length === 0 && isSubmit) {
+      console.log(formValues);
+    }
+  }, [formErrors]);
+  const validate = (values) => {
+    const errors = {};
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+    if (!values.email) {
+      errors.email = <>{t("login9")}</>;
+    } else if (!regex.test(values.email)) {
+      errors.email = <>{t("login10")}</>;
+    }
+    if (!values.password) {
+      errors.password = <>{t("login11")}</>;
+    } else if (values.password.length < 8) {
+      errors.password = <>{t("login12")}</>;
+    } else if (backendError !== "") {
+      errors.password = <>{backendError}</>;
+    }
+
+    return errors;
+  };
+  if (userInfo === null) {
+    return (
+      <div className="full_login">
+        <div className="Log_In_form">
+          <form onSubmit={handleSubmit}>
+            {/* <div className="ui divider"></div> */}
+            <div className="ui_form">
+              <div className="field">
+                <input
+                  id="input_login"
+                  type="text"
+                  name="email"
+                  placeholder={t("login3")}
+                  value={formValues.email}
+                  onChange={(e) => handleChange(e)}
+                />
+                <p className="error_login">{formErrors.email}</p>
+              </div>
+              <div className="field">
+                <input
+                  id="input_login"
+                  type="password"
+                  name="password"
+                  placeholder={t("login4")}
+                  value={formValues.password}
+                  onChange={(e) => handleChange(e)}
+                />
+                <p className="error_login">{formErrors.password}</p>
+              </div>
+              <Link className="forgetpassword_login" to="/forgotpassword">
+                {t("login5")}
+              </Link>
+            </div>
+            <button className="acount_button_login">{t("login6")}</button>
+          </form>
+          <h4 className="p_login">{t("login7")} </h4>
+          <Link to="google.com" className="GoogleBtn">
+            <button className="google_button_singup">
+              <FaGoogle className="google_icon_login" />
+              <div className="google_p_login">{t("login8")}</div>
+            </button>
+          </Link>
+        </div>
+        <div className="mobilee">
+          <form onSubmit={handleSubmit}>
+            <div className="ui_form">
+              <div className="field">
+                <input
+                  id="input_login"
+                  type="text"
+                  name="email"
+                  placeholder={t("login3")}
+                  value={formValues.email}
+                  onChange={handleChange}
+                />
+                <p className="error_login">{formErrors.email}</p>
+              </div>
+              <div className="field">
+                <input
+                  id="input_login"
+                  type="password"
+                  name="password"
+                  placeholder={t("login4")}
+                  value={formValues.password}
+                  onChange={handleChange}
+                />
+                <p className="error_login">{formErrors.password}</p>
+              </div>
+              <Link className="forgotpassword_login" to="/forgotpassword">
+                {t("login5")}
+              </Link>
+            </div>
+            <button className="acount_button_login">{t("login6")}</button>
+          </form>
+          <h4 className="p_login">{t("login7")} </h4>
+          <Link to="google.com" className="GoogleBtn">
+            <button className="google_button_login" onClick={login}>
+              <FaGoogle className="google_icon_login" />
+              <div className="google_p_login">{t("login8")}</div>
+            </button>
+          </Link>
+        </div>
+      </div>
+    );
+  } else if (userInfo !== null) {
+    return navigate("/header");
+  } else if (isLoading) {
+    return <h1>Loading.....</h1>;
+  }
+};
+export default withNamespaces()(LogIn);
